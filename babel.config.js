120fc2a728a174818f1cd314c85733b020498562
gin1231 <jineok12@gmail.com>
2022-07-15 01:11:45 +0900

    added dependency injection

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }]],
   plugins: [
+    'babel-plugin-transform-typescript-metadata',
     [
       '@babel/plugin-transform-react-jsx',
       {
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'reflect-metadata';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
