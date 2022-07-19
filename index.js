9a0c77fbba2725830e45a3523b973e58b868ae0a
gin1231 <jineok12@gmail.com>
2022-07-19 17:20:02 +0900

    bind server share fetching logic

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ import { AppRegistry } from 'react-native';
 
 import { name as appName } from './app.json';
 import App from './src/App';
+import { setAxiosConfig } from './src/utils/request';
+
+setAxiosConfig({
+  baseURL: '***REMOVED***',
+  headers: {
+    Authorization: 'Basic bXZsLWNsdXRjaC0yYTg5ZDJlNGYxZGRhOGQ4OndhbGxldC1zZWNyZXQtMzMwYmM4ODVmNDA5NmE0MQ==',
+  },
+});
 
 AppRegistry.registerComponent(appName, () => App);
--- src/domain/auth/auth.ts
+++ src/domain/auth/auth.ts
@@ -1,4 +1,5 @@
 import { ShareStore } from '@tkey/common-types';
+import { ShareStoreMap } from '@tkey/common-types/src/base/ShareStore';
 import ThresholdKey from '@tkey/core';
 import PrivateKeyModule, { SECP256k1Format } from '@tkey/private-keys';
 import { PRIVATE_KEY_MODULE_NAME } from '@tkey/private-keys/src/PrivateKeys';
@@ -9,6 +10,9 @@ import TorusStorageLayer from '@tkey/storage-layer-torus';
 // @ts-ignore
 import CustomAuth from '@toruslabs/customauth-react-native-sdk';
 import BN from 'bn.js';
+import qs from 'qs';
+
+import { request } from '../../utils/request';
 
 const enableLogging = process.env.ENABLE_TORUS_LOGGING === 'true';
 
@@ -33,15 +37,20 @@ const seedPhraseModule = new SeedPhraseModule([new MetamaskSeedPhraseFormat('htt
 const storageLayer = new TorusStorageLayer({ hostUrl: 'https://metadata.tor.us' });
 
 export class CustomAuthImpl implements Auth {
-  private async fetchServerShare(): Promise<ShareStore> {
-    return ShareStore.fromJSON({
-      share: {
-        share: '2cec8588aa5bad477ebe3a69328b1a4226f3d2017f99312d44e4bf8c9c2e4df6',
-        shareIndex: 'd0b4f68a80494ef651ce09629406f0af25e25301d262807a2f50ac6b5f580943',
-      },
-      polynomialID:
-        '039a3f34da6780ed048d871e876db4fd5a3f6e85e3330dae0932be5713a73e4ebb|024dc508b004f7b61e3926d9da2d74f66953835c6708dcbba961a48eeb0af746b8',
-    });
+  private static async fetchServerShare(provider: 'GOOGLE' | 'APPLE', idToken: string, polynomialId: string): Promise<ShareStore | null> {
+    try {
+      const endpoint = `/v1/accounts/ss?${qs.stringify({
+        provider: provider,
+        idtoken: idToken,
+        polynomialId: polynomialId,
+      })}`;
+      console.log(endpoint);
+      const res = await request.get(endpoint);
+      console.log(res);
+      return ShareStore.fromJSON(JSON.parse(res.data.jsonString));
+    } catch (e) {
+      return null;
+    }
   }
 
   private async fetchDeviceShare(): Promise<ShareStore> {
@@ -72,7 +81,6 @@ export class CustomAuthImpl implements Auth {
       });
 
       const postboxKey = credentials.privateKey;
-      console.log(`postboxKey: ${postboxKey}`);
 
       const serviceProvider = serviceProviderWithPostboxKey(postboxKey);
 
@@ -84,13 +92,31 @@ export class CustomAuthImpl implements Auth {
           [SEED_PHRASE_MODULE_NAME]: seedPhraseModule,
         },
       });
+      await tKey.initialize({ neverInitializeNewKey: true });
       console.log(tKey);
 
-      const serverShare = await this.fetchServerShare();
-      tKey.inputShareStore(serverShare);
-      console.log(serverShare);
+      let inputShare: ShareStore | null = null;
+      for (const polyId in tKey.metadata.publicPolynomials) {
+        const shareStoreMap: ShareStoreMap | undefined = tKey.shares[polyId];
+        if (shareStoreMap === undefined) {
+          continue;
+        }
+        const torusShare: ShareStore | undefined = shareStoreMap['1'];
+        if (torusShare === undefined) {
+          continue;
+        }
+        const serverShare = await CustomAuthImpl.fetchServerShare('GOOGLE', credentials.userInfo.idToken, torusShare.polynomialID);
+        if (serverShare !== null) {
+          inputShare = serverShare;
+          break;
+        }
+      }
+      if (inputShare === null) {
+        throw new Error('no server share');
+      }
+      tKey.inputShareStore(inputShare);
+      console.log(inputShare);
 
-      await tKey.initialize({ neverInitializeNewKey: true });
       console.log(`tKey.init`);
       const res = await tKey.reconstructKey();
       console.log(`reconstructed: ${res}`, res);
