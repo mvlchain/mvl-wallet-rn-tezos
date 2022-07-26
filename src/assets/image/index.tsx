44367a0baace8c38322790f1e2161bfe53e1ed54
Yongjun Park <pyjun02@gmail.com>
2022-07-26 18:43:44 +0900

    Add assets

---
+++ src/assets/image/index.tsx
@@ -0,0 +1,15 @@
+export { default as BrowserNormal } from './bottom-tab-icon/browser-normal.svg';
+export { default as BrowserSelected } from './bottom-tab-icon/browser-selected.svg';
+export { default as HomeNormal } from './bottom-tab-icon/home-normal.svg';
+export { default as HomeSelected } from './bottom-tab-icon/home-selected.svg';
+export { default as SettingNormal } from './bottom-tab-icon/setting-normal.svg';
+export { default as SettingSelected } from './bottom-tab-icon/setting-selected.svg';
+export { default as TradeNormal } from './bottom-tab-icon/trade-normal.svg';
+export { default as TradeSelected } from './bottom-tab-icon/trade-selected.svg';
+
+export { default as MoreIcon } from './ic-more.svg';
+export { default as CopyIcon } from './ic-copy.svg';
+export { default as ChevronDownBlueIcon } from './ic-chevron-down-blue.svg';
+export { default as ChevronDownBlackIcon } from './ic-chevron-down-black.svg';
+export { default as WhiteQrIcon } from './ic-qr-white.svg';
+export { default as WhiteScanIcon } from './ic-scan-white.svg';
