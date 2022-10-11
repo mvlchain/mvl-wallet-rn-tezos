9a26ae4158c4a7f1765f067ca40edaa08309b6f8
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-10-11 17:55:10 +0900

    fix: add asset

--- src/assets/image/index.tsx
+++ src/assets/image/index.tsx
@@ -20,6 +20,8 @@ export { default as HideLight } from './ic-hide-light.svg';
 export { default as TextFieldDelete } from './ic-text-field-delete.svg';
 export { default as PasswordDeleteDark } from './ic-password-delete-black-dark.svg';
 export { default as PasswordDeleteLight } from './ic-password-delete-black-light.svg';
+export { default as CloseBlackIconDark } from './ic-close-black-dark.svg';
+export { default as CloseBlackIconLight } from './ic-close-black-light.svg';
 
 export { default as BiometricSmallDark } from './img-biometirc-s-light.svg';
 export { default as BiometricSmallLight } from './img-biometirc-s-light.svg';
@@ -29,5 +31,6 @@ export { default as Simple } from './img-simple.svg';
 export { default as SocialAppleDark } from './img-social-apple-dark.svg';
 export { default as SocialAppleLight } from './img-social-apple-light.svg';
 export { default as SocialGoogle } from './img-social-google.svg';
+export { default as NFTImage } from './img-nft.svg';
 
 export { default as TokenMVL32Icon } from './ic-token-mvlx32.svg';
