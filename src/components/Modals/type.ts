18bb208d94b2646b266b80cbf798c804dbabb8be
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-10-11 18:48:05 +0900

    fix: modal component

--- src/components/Modals/type.ts
+++ src/components/Modals/type.ts
@@ -1,11 +1,20 @@
 import React from 'react';
 
+export const Center = 'center' as const;
+export const Bottom = 'bottom' as const;
+
+export type ModalType = typeof Center | typeof Bottom;
 export interface ModalComponentProps {
+  type: ModalType;
   title: string;
-  children?: React.ReactNode;
+  children: React.ReactNode;
+  isVisible: boolean;
   onCancel?: Function;
   onConfirm?: Function;
   onClose?: Function;
-  isVisible: boolean;
-  type?: 'middle' | 'bottom';
+  image?: React.ReactNode;
+}
+
+export interface ModalTypeProps {
+  type: ModalType;
 }
