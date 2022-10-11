d7600b95f8bde959cd7c16a55d17ff0e3e63d1df
cudymvl <cudy.kim@mvlchain.io>
2022-10-11 21:56:05 +0900

    feat: seedphrase기본 틀 작업

---
+++ src/screens/Mnemonic/ConfirmSeedPhrase/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+function ConfirmSeedPhrase() {
+  return <Text>ConfirmSeedPhrase</Text>;
+}
+
+export default ConfirmSeedPhrase;
---
+++ src/screens/Mnemonic/SeedPhrase/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { useNavigation } from '@react-navigation/native';
+
+import { ROUTE_NAME } from '@@assets/constants';
+import { PrimaryButton } from '@@components/Buttons/BaseButton';
+import { TextButton } from '@@components/Buttons/TextButton';
+import Mnemonic from '@@components/Mnemonic/Mnemonic';
+import { MainTabParamList } from '@@types/MainTabType';
+import { height } from '@@utils/ui';
+
+import * as STC from '../Mnemonic.style';
+
+const mnemonic =
+  'legend shop meadow arch border eagle river puppy earth express turn media debris brass kite error daughter food option bag extra blame food year';
+type seedPhraseProp = BottomTabNavigationProp<MainTabParamList, 'SEEDPHRASE'>;
+
+function SeedPhrase() {
+  const [type, setType] = useState<'show' | 'hide'>('show');
+  const navigation = useNavigation<seedPhraseProp>();
+  return (
+    <STC.Container bounces={false}>
+      <STC.Description>
+        {`Keep this Seed Phrase in a safe and secret place.\n
+        DO NOT reveal this phrase to other people!\n
+        Clutch team never request you to share this Seed Phrase.`}
+      </STC.Description>
+      <Mnemonic type={'show'} mnemonic={mnemonic} />
+      {type && (
+        <>
+          <TextButton
+            label='Copy to Clipboard'
+            onPress={() => console.log('hello')}
+            disabled={false}
+            wrapperStyle={{ marginTop: height * 30, marginBottom: height * 18 }}
+          />
+          <PrimaryButton label='Next' onPress={() => navigation.navigate(ROUTE_NAME.CONFIRM_SEEDPHRASE)} disabled={false} />
+        </>
+      )}
+    </STC.Container>
+  );
+}
+
+export default SeedPhrase;
