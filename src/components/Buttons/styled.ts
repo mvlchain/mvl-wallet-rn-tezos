eedc5bd37f0e2a4d34328c304cbb5ba4758ded9c
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2022-10-12 14:47:37 +0900

    fix: basic component  styled component import statement fix

--- src/components/Buttons/styled.ts
+++ src/components/Buttons/styled.ts
@@ -1,4 +1,4 @@
-import Styled, { css } from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 import {
   BaseButtonWrapper,
@@ -11,31 +11,31 @@ import {
 } from './type';
 
 //Base
-export const BaseButtonContainer = Styled.Pressable<BaseButtonWrapper>`
-${({ size }) =>
-  size === BUTTON_SIZE.SMALL
-    ? null
-    : css`
-        width: 100%;
-      `};
+export const BaseButtonContainer = styled.Pressable<BaseButtonWrapper>`
+  ${({ size }) =>
+    size === BUTTON_SIZE.SMALL
+      ? null
+      : css`
+          width: 100%;
+        `};
 `;
-export const BaseButton = Styled.View<BaseButtonProps>`
+export const BaseButton = styled.View<BaseButtonProps>`
   width: 100%;
   align-items: center;
   justify-content: center;
   border-radius: 8px;
   height: ${({ size }) => (size === BUTTON_SIZE.SMALL ? '40px' : '60px')};
-  padding: 12px 16px ;
-  background-color:${({ theme, pressed, disabled, bgColor, bgColorPressed, bgColorDisabled }) =>
+  padding: 12px 16px;
+  background-color: ${({ theme, pressed, disabled, bgColor, bgColorPressed, bgColorDisabled }) =>
     disabled ? theme.color[bgColorDisabled] : !disabled && pressed ? theme.color[bgColorPressed] : theme.color[bgColor]};
-  border-color:${({ theme, lcColor }) => (lcColor ? theme.color[lcColor] : 'transparent')};
+  border-color: ${({ theme, lcColor }) => (lcColor ? theme.color[lcColor] : 'transparent')};
   border-style: solid;
   border-width: 1px;
 `;
-export const BaseButtonLabel = Styled.Text<BaseButtonLabelProps>`
+export const BaseButtonLabel = styled.Text<BaseButtonLabelProps>`
   ${({ theme, size }) => (size === BUTTON_SIZE.SMALL ? theme.font.Label.sm : theme.font.Label.lg)};
   color: ${({ theme, disabled, txColor, txColorDisabled }) => (disabled ? theme.color[txColorDisabled] : theme.color[txColor])};
-  font-Family:${({ theme }) => theme.fmRegular};
+  font-family: ${({ theme }) => theme.fmRegular};
 `;
 
 export const baseButtonStyleObj: BaseButtonStyleObj = {
@@ -87,30 +87,30 @@ export const baseButtonStyleObj: BaseButtonStyleObj = {
 };
 
 //Social
-export const SocialButton = Styled(BaseButton)`
+export const SocialButton = styled(BaseButton)`
   flex-direction: row;
 `;
-export const BoldLabel = Styled(BaseButtonLabel)`
+export const BoldLabel = styled(BaseButtonLabel)`
   font-family: ${({ theme }) => theme.fmBold};
 `;
 
-export const IconWrapper = Styled.View<IconWrapperProps>`
+export const IconWrapper = styled.View<IconWrapperProps>`
   position: absolute;
   left: 16px;
   opacity: ${({ disabled }) => (disabled ? 0.2 : 1)};
 `;
-export const TextWrapper = Styled.View`
- flex-direction: row;
+export const TextWrapper = styled.View`
+  flex-direction: row;
 `;
 
 //Text
-export const TextButtonContainer = Styled.Pressable`
+export const TextButtonContainer = styled.Pressable`
   align-items: center;
   justify-content: center;
   border-radius: 8px;
 `;
 
-export const TextButtonLabel = Styled.Text<TextButtonLabelProps>`
+export const TextButtonLabel = styled.Text<TextButtonLabelProps>`
   ${({ theme }) => theme.font.Label.sm};
   color: ${({ theme, disabled }) => (disabled ? theme.color.primaryDarkest : theme.color.primary)};
   background-color: ${({ theme, pressed }) => (pressed ? theme.color.grey100Grey900 : 'transparent')};
--- src/components/TextFields/styled.ts
+++ src/components/TextFields/styled.ts
@@ -1,4 +1,4 @@
-import Styled from 'styled-components/native';
+import styled from 'styled-components/native';
 
 import { fontSize } from '@@utils/ui';
 
@@ -7,99 +7,98 @@ import * as Type from './type';
 //Base
 //height를 어떻게 해야하지? 글씨크기를 상대적으로 할거면 모두다 그렇게 해야하는거 아닌가
 const inputHeight = fontSize(48) + 'px';
-export const BaseInput = Styled.TextInput<Type.BaseTextFieldProps>`
-   flex:9;
+export const BaseInput = styled.TextInput<Type.BaseTextFieldProps>`
+  flex: 9;
 `;
-export const BaseTextFieldInputWrapper = Styled.View<Type.ContainerProps>`
-    width: 100%;
-    flex-direction: row;
-    border-style : solid;
-    border-width : 1px;
-    border-color : ${({ theme, lcColor }) => (lcColor ? lcColor : theme.color.grey100Grey900)};
-    border-radius : 8px;
-    ${({ theme }) => theme.font.Paragraph.md};
-    height: ${inputHeight};
-    padding: 14px 16px;
-    gap: 8px;
-    outline-color: ${({ theme }) => theme.color.primary};
-    outline-style: solid;
+export const BaseTextFieldInputWrapper = styled.View<Type.ContainerProps>`
+  width: 100%;
+  flex-direction: row;
+  border-style: solid;
+  border-width: 1px;
+  border-color: ${({ theme, lcColor }) => (lcColor ? lcColor : theme.color.grey100Grey900)};
+  border-radius: 8px;
+  ${({ theme }) => theme.font.Paragraph.md};
+  height: ${inputHeight};
+  padding: 14px 16px;
+  gap: 8px;
+  outline-color: ${({ theme }) => theme.color.primary};
+  outline-style: solid;
 `;
 
-export const Unit = Styled.Text`
+export const Unit = styled.Text`
   color: ${({ theme }) => theme.color.grey300};
   line-height: 20px;
   margin-left: 8px;
 `;
 
-export const BaseTextFieldLabel = Styled.Text`
+export const BaseTextFieldLabel = styled.Text`
   color: ${({ theme }) => theme.color.blackWhite};
   line-height: 20px;
   margin-bottom: 8px;
 `;
 
-export const BaseTextFieldHint = Styled.Text`
-color: ${({ theme }) => theme.color.grey500};
-line-height: 20px;
-margin-top: 8px;
+export const BaseTextFieldHint = styled.Text`
+  color: ${({ theme }) => theme.color.grey500};
+  line-height: 20px;
+  margin-top: 8px;
 `;
 
-export const BaseTextFieldContainer = Styled.View`
-`;
+export const BaseTextFieldContainer = styled.View``;
 
 //TradeVolume
-export const TradeVolumeContainer = Styled.View`
-width: 295px;
-background-color: ${({ theme }) => theme.color.white};
-border-style: solid;
-border-width: 1px;
-border-color:  ${({ theme }) => theme.color.grey100};
-border-radius: 8px;
-padding: 16px;
+export const TradeVolumeContainer = styled.View`
+  width: 295px;
+  background-color: ${({ theme }) => theme.color.white};
+  border-style: solid;
+  border-width: 1px;
+  border-color: ${({ theme }) => theme.color.grey100};
+  border-radius: 8px;
+  padding: 16px;
 `;
 
-export const TradeVolumeTop = Styled.View`
-width:100%;
-flex-direction: row;
-justify-content: space-between;
-margin-bottom: 16px;
+export const TradeVolumeTop = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 16px;
 `;
-export const TradeVolumeMiddle = Styled.View`
-width:100%;
-flex-direction: row;
-align-items: center;
-`;
-
-export const From = Styled.Text`
-${({ theme }) => theme.font.Paragraph.md};
-color: ${({ theme }) => theme.color.grey500};
-line-height: 20px;
+export const TradeVolumeMiddle = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
 `;
 
-export const Hint = Styled.Text`
-${({ theme }) => theme.font.Paragraph.md};
-color: ${({ theme }) => theme.color.grey500};
-line-height: 20px;
-margin-top: 16px;
-`;
-export const Token = Styled.Text`
-${({ theme }) => theme.font.Label.md};
-margin-left: 8px;
-line-height: 20px;
+export const From = styled.Text`
+  ${({ theme }) => theme.font.Paragraph.md};
+  color: ${({ theme }) => theme.color.grey500};
+  line-height: 20px;
 `;
 
-export const TradeVolumeInputWrapper = Styled.View`
-flex: 1.5;
-flex-direction: row;
-align-items: center;
-margin-right: 16px;
+export const Hint = styled.Text`
+  ${({ theme }) => theme.font.Paragraph.md};
+  color: ${({ theme }) => theme.color.grey500};
+  line-height: 20px;
+  margin-top: 16px;
 `;
-export const TradeVolumeInput = Styled.TextInput`
-flex:1;
-${({ theme }) => theme.font.Label.md};
-line-height: 20px;
+export const Token = styled.Text`
+  ${({ theme }) => theme.font.Label.md};
+  margin-left: 8px;
+  line-height: 20px;
 `;
-export const SymbolWrapper = Styled.View`
-flex-direction: row;
-justify-content: flex-end; 
-align-items: center;
+
+export const TradeVolumeInputWrapper = styled.View`
+  flex: 1.5;
+  flex-direction: row;
+  align-items: center;
+  margin-right: 16px;
+`;
+export const TradeVolumeInput = styled.TextInput`
+  flex: 1;
+  ${({ theme }) => theme.font.Label.md};
+  line-height: 20px;
+`;
+export const SymbolWrapper = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
 `;
