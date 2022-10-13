e40fea1c8bd8ae5b295453d3a64a2d1b08717885
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-10-13 20:18:14 +0900

    fix: global modal type fix

---
+++ src/store/globalModal/useGlobalModalStore.ts
@@ -0,0 +1,15 @@
+import create from 'zustand';
+
+import { MODAL_TYPE, GlobalModalStore, MODAL_PROPS } from './types';
+const useGlobalModalStore = create<GlobalModalStore>((set, get) => ({
+  modalStack: [],
+  modalType: null,
+  modalProps: null,
+  openModal: <K extends MODAL_TYPE>(modalType: K, modalProps: MODAL_PROPS<K>) => {
+    set({ modalType, modalProps });
+  },
+  closeModal: () => {
+    set({ modalType: null, modalProps: null });
+  },
+}));
+export default useGlobalModalStore;
