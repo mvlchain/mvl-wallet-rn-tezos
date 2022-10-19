0043b94a634fabc641154b4894a33fb2fdf0e1fd
cudymvl <cudy.kim@mvlchain.io>
2022-10-19 11:47:16 +0900

    feat: app version 기본작업

--- src/screens/Setting/SettingAppVersion/SettingAppVersion.tsx
+++ src/screens/Setting/SettingAppVersion/SettingAppVersion.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 
 import { Text, View } from 'react-native';
 
+import { ClutchImageLight, ClutchImageDark } from '@@assets/image';
+import { SecondaryButton } from '@@components/BasicComponents/Buttons/BaseButton';
+import { useAssetFromTheme } from '@@hooks/common/useTheme';
+
+import * as S from './SettingAppVersion.style';
+/**
+ * TODO:
+ * 1. 앱 버전 확인 후 rendering
+ * 2. 앱 버전에 따른 버튼 label, onPress, disabled수정
+ */
 function SettingAppVersion() {
+  const ClutchImage = useAssetFromTheme(ClutchImageLight, ClutchImageDark);
   return (
-    <View>
-      <Text>Setting AppVersion</Text>
-    </View>
+    <S.AppVersionContainer>
+      <S.ClutchImageContainer>
+        <ClutchImage />
+      </S.ClutchImageContainer>
+      <S.ClutchButtonContainer>
+        <SecondaryButton onPress={() => console.log('TODO: 앱버전 확인하기')} label={'Check for Update'} />
+      </S.ClutchButtonContainer>
+      <S.AppText>Version 1.0</S.AppText>
+    </S.AppVersionContainer>
   );
 }
 
