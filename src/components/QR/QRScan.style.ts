8030d0e890230128611e5e8dd298ff6dd1003014
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-10-21 18:50:26 +0900

    fix: style convention apply

--- src/components/QR/QRScan.style.ts
+++ src/components/QR/QRScan.style.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components/native';
 import { height, width } from '@@utils/ui';
 
 export const QRScanTopContainer = styled.View`
-  height: ${height * 172}px;
-  background-color: ${({ theme }) => theme.color.whiteBlack};
-  padding-bottom: ${width * 24}px;
   justify-content: flex-end;
   align-items: center;
+  height: ${height * 172}px;
+  padding-bottom: ${width * 24}px;
+  background-color: ${({ theme }) => theme.color.whiteBlack};
 `;
 export const QRScanTopText = styled.Text`
   ${({ theme }) => theme.font.Paragraph.md}
@@ -16,11 +16,11 @@ export const QRScanTopText = styled.Text`
 
 export const QRScanGalleryButtonWrapper = styled.View`
   position: absolute;
-  width: 100%;
   bottom: ${height * 58}px;
+  z-index: 4;
+  width: 100%;
   padding-right: ${width * 24}px;
   padding-left: ${width * 24}px;
-  z-index: 4;
 `;
 export const QRScanCameraWrapper = styled.View`
   height: 100%;
@@ -32,8 +32,8 @@ export const QRScanContainer = styled.View`
 
 export const QRScanOverlayWrapper = styled.View`
   position: absolute;
-  width: 100%;
-  height: 100%;
   top: ${height * 172}px;
   z-index: 3;
+  width: 100%;
+  height: 100%;
 `;
