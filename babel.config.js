1a5e40cbf8172f5f654b48f3e75d6d97ff0f52ed
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-10-24 15:38:48 +0900

    hotfix: delete duplicated babel config

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: [['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }]],
   plugins: [
     'babel-plugin-transform-typescript-metadata',
-    'react-native-reanimated/plugin',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       '@babel/plugin-transform-react-jsx',
