89a3b5f33562d5547895e6d59a824e4cd368ec7f
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-10-24 18:52:53 +0900

    fix: qr function wip

--- src/hooks/qr/useQR.ts
+++ src/hooks/qr/useQR.ts
@@ -1,101 +1,74 @@
 import { useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
 import { NativeModules } from 'react-native';
 import { launchImageLibrary } from 'react-native-image-picker';
-import { runOnJS } from 'react-native-reanimated';
+import { runOnJS, useDerivedValue } from 'react-native-reanimated';
 import { Frame, useFrameProcessor } from 'react-native-vision-camera';
 import { BarcodeFormat, scanBarcodes, Barcode } from 'vision-camera-code-scanner';
 
+import { IQRcode } from '@@components/QR/QRcode/QRcode.type';
 import globalModalStore from '@@store/globalModal/globalModalStore';
 
 const useQR = (targetToken?: string) => {
-  const [QRs, setQRs] = useState<Barcode[] | []>([]);
+  const [QRs, setQRs] = useState<Barcode[] | null>();
   const { openModal } = globalModalStore();
-  const { navigation } = useNavigation<any>();
+  const { t } = useTranslation();
 
-  const goSendPage = () => {
-    navigation.navigate('send');
-  };
-
-  const openGallery = async () => {
-    try {
-      const result = await launchImageLibrary({ mediaType: 'photo' });
-
-      if (result.didCancel) {
-        //TODO: modal "user cancel"
-        return;
-      }
-      if (!result || !result.assets || !result.assets[0].uri) {
-        //TODO: setting과 합친 후 모달열기 "result.errormessage or error gallery"
-        return;
-      }
-      const imgUri = result.assets[0].uri;
-      return imgUri;
-    } catch (err) {
-      //TODO: setting과 합친 후 모달열기 err.message
-      return;
-    }
-  };
-
-  const QRreader = async (fileUrl: string) => {
-    try {
-      var QRScanReader = NativeModules.QRScanReader;
-      return await QRScanReader.readerQR(fileUrl);
-    } catch (err) {
-      //TODO: setting과 합친 후 모달열기 err.message
-      return;
-    }
-  };
-
+  //TODO: figma 에는 작업되어 있지않은데 토큰을 선택하고 qr을 인식하는 경우에 기존 선택한 qr과 인식된 qr코드의 토큰이 다른경우에 유저한테 바꿀건지 다시 인식할건지 물어봐야함
   const isSameToken = (targetToken: string, token: string) => {
     if (targetToken !== token) {
       //confirm 모달
     }
   };
 
-  const isValidQR = (readOrScanData: string) => {
-    if (!readOrScanData.includes('token')) return false;
-    if (!readOrScanData.includes('address')) return false;
-    if (!readOrScanData.includes('amount')) return false;
-    return true;
+  const goSendPage = (scanResult: any) => {
+    console.log(scanResult);
+
+    if (!scanResult.hasOwnProperty('token') || !scanResult.hasOwnProperty('address') || !scanResult.hasOwnProperty('amount')) {
+      openModal('TITLE_ONLY', t('dialog_wrong_qr_code_title'));
+      return;
+    }
+
+    const parsedData = JSON.parse(scanResult);
+    console.log(parsedData);
   };
 
   const getQRFromGallery = async () => {
     try {
-      const imgUri = await openGallery();
-      const scanResult = await QRreader(imgUri as string);
+      const result = await launchImageLibrary({ mediaType: 'photo' });
+      if (!result || result.didCancel || !result.assets || !result.assets[0].uri) {
+        return;
+      }
+      const imgUri = result.assets[0].uri;
+      console.log('imgUri', imgUri);
+      const scanResult = await NativeModules.QRScanReader.readerQR(imgUri);
+      console.log('scanResult', scanResult);
+      if (!scanResult.includes('token') || !scanResult.includes('address') || !scanResult.includes('amount')) {
+        openModal('TITLE_ONLY', t('dialog_wrong_qr_code_title'));
+        return;
+      }
+      goSendPage(scanResult);
     } catch (err) {
-      //TODO: setting과 합친 후 모달열기 err.message
       return;
     }
   };
 
-  const isInSafeScanArea = (frame: Frame, barcodes: Barcode[]) => {
-    if (!barcodes || !barcodes[0].cornerPoints) return;
-    // TODO: 스캔 제한영역 설정
-    const [topL, bottomL, bottomR, topR] = barcodes[0].cornerPoints;
-    if (topL.x < frame.width * 0.1 || topL.y < frame.height * 0.12 || bottomR.x > frame.width * 0.9 || bottomR.y > frame.width * 0.6) return false;
-    return true;
-  };
-
-  const scan = useFrameProcessor((frame) => {
+  const frameProcessor = useFrameProcessor((frame) => {
     'worklet';
     const detectedBarcodes = scanBarcodes(frame, [BarcodeFormat.QR_CODE], { checkInverted: true });
-    if (!isInSafeScanArea(frame, detectedBarcodes)) {
-      return;
-    }
-    const scanResult = detectedBarcodes[0].content.data.toString();
-    if (!isValidQR(scanResult)) {
-      return;
-    }
-    //TODO: set이 아니라 페이지로 보내야함
-    runOnJS(setQRs)(detectedBarcodes);
+    // scan area limit in overlay area
+    if (!detectedBarcodes || !detectedBarcodes[0] || !detectedBarcodes[0].cornerPoints) return;
+    const [topL, bottomL, bottomR, topR] = detectedBarcodes[0].cornerPoints;
+    if (topL.x < frame.width * 0.1 || topL.y < frame.height * 0.12 || bottomR.x > frame.width * 0.9 || bottomR.y > frame.width * 0.6) return;
+
+    runOnJS(goSendPage)(detectedBarcodes[0].content.data);
   }, []);
 
   return {
     getQRFromGallery,
-    scan,
+    frameProcessor,
   };
 };
 
