9387b6f878aa488fa192c9ff1675871023b6e869
cudymvl <cudy.kim@mvlchain.io>
2022-10-25 18:51:53 +0900

    fix: error boundaryscreen component 이름 수정

--- src/screens/ErrorBoundaryScreen/ErrorBoundaryScreen.tsx
+++ src/screens/ErrorBoundaryScreen/ErrorBoundaryScreen.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { Text } from 'react-native';
 
-function ErrorBoundary() {
+function ErrorBoundaryScreen() {
   // TODO: 화면에 맞게 수정
   return <Text>Something went wrong</Text>;
 }
 
-export default ErrorBoundary;
+export default ErrorBoundaryScreen;
