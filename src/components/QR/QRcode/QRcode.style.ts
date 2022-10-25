2758d44991469f2a76c82b3d3497c84dc401cb6f
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-10-25 16:10:16 +0900

    fix: style convention apply

--- src/components/QR/QRcode/QRcode.style.ts
+++ src/components/QR/QRcode/QRcode.style.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components/native';
 import { width, height } from '@@utils/ui';
 
 export const QRcodeWrapper = styled.View`
+  justify-content: center;
+  align-items: center;
   width: ${width * 240}px;
   height: ${height * 240}px;
   background-color: white;
-  justify-content: center;
-  align-items: center;
 `;
