6885df65e1a5b1fda448d3fbacab68bef0366948
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-11-16 21:07:55 +0900

    fix: setHistory 작업 중

--- src/store/transaction/transactionStore.type.ts
+++ src/store/transaction/transactionStore.type.ts
@@ -1,6 +1,6 @@
-import { ITransaction } from '@@domain/transaction/TransactionService.type';
+import { IFetchTransactionHistoryResponse, ITransaction } from '@@domain/transaction/TransactionService.type';
 export type TTransactionStoreState = Record<string, Array<ITransaction>>;
 
 export interface ITransactionStore {
-  setHistory: (token: string, history: Array<ITransaction>) => void;
+  setHistory: (token: string, history: Array<IFetchTransactionHistoryResponse>, beforeblock: number, beforeindex: number) => void;
 }
