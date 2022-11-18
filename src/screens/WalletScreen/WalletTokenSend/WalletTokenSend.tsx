e625ceb449f4ec8a1d8afd9886bd9b6c1784756f
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2022-11-18 16:19:21 +0900

    fix:  send screen view

--- src/screens/WalletScreen/WalletTokenSend/WalletTokenSend.tsx
+++ src/screens/WalletScreen/WalletTokenSend/WalletTokenSend.tsx
@@ -1,9 +1,14 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { useTranslation } from 'react-i18next';
 import { Text, View } from 'react-native';
+import { add } from 'react-native-reanimated';
 
+import Divider from '@@components/BasicComponents/Divider';
+import { DIVIDER_THICKNESS } from '@@components/BasicComponents/Divider/Divider.type';
+import SendInputBoard from '@@components/WalletTokenSend/SendInputBoard';
+import SpeedOperationBoard from '@@components/WalletTransactionSpeedUpCancel/SpeedOperationBoard';
 import useHeader from '@@hooks/common/useHeader';
 
 import { TTokenSendRootStackProps, TTokenSendRouteProps } from './WalletTokenSend.type';
@@ -18,9 +23,14 @@ function WalletTokenSend() {
     const headerOption = handleStackHeaderOption({ title: t('send') });
     navigation.setOptions(headerOption);
   }, []);
+
+  const [amount, setAmount] = useState('0');
+  const [address, setAddress] = useState('');
   return (
-    <View>
-      <Text>Wallet Token Send</Text>
+    <View style={{ flex: 1 }}>
+      <SendInputBoard amount={amount} setAmount={setAmount} address={address} setAddress={setAddress} />
+      <Divider thickness={DIVIDER_THICKNESS.THICK} />
+      <SpeedOperationBoard />
     </View>
   );
 }
