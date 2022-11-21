91d848eba07aa6e76ae959f863f639c96a38fd06
Darby <cutehackers@gmail.com>
2022-11-21 10:25:13 +0900

    fix: App component testing failure case by adding RTNSettingsRepository

---
+++ __mocks__/react-native-splash-screen/index.js
@@ -0,0 +1,5 @@
+
+export default {
+  show: jest.fn().mockImplementation(() => { console.log('show splash screen'); }),
+  hide: jest.fn().mockImplementation(() => { console.log('hide splash screen'); }),
+}
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,22 +3,45 @@ import React from 'react';
 
 import App from './App';
 import { act, cleanup, render } from './test/test-utils';
-import './di/di';
+
+import { container, instancePerContainerCachingFactory, injectable } from 'tsyringe';
+
 import 'jest-styled-components';
+import { RTNSettingsRepository } from '@@domain/auth/repositories/RTNSettingsRepository';
+import { TTheme } from '@@store/setting/settingPersistStore.type';
+
+/**
+ * Mock class for RTNSettingsRepository
+ */
+@injectable()
+class MockRTNSettingsRepository implements RTNSettingsRepository {
+  constructor() {}
+
+  getThemeType(): TTheme {
+    return 'light';
+  }
+  putThemeType(themeType: TTheme): void {}
+}
 
 jest.useFakeTimers();
 
 // Note: test renderer must be required after react-native.
-
 afterEach(cleanup);
 
-// mock splash screen
-jest.mock('react-native-splash-screen', () => {
-  return {
-    hide: jest.fn(),
-    show: jest.fn(),
-  };
+beforeEach(() => {
+  // mock splash screen
+  jest.mock('react-native-splash-screen', () => {
+    return {
+      hide: jest.fn(),
+      show: jest.fn(),
+    };
+  });
 });
+
+container.register('RTNSettingsRepository', {
+  useFactory: instancePerContainerCachingFactory<MockRTNSettingsRepository>((container) => container.resolve(MockRTNSettingsRepository)),
+});
+//const settingsRepository: RTNSettingsRepository = container.resolve('RTNSettingsRepository');
 
 test('renders correctly', async () => {
   render(<App />);
