2d91a86329f086af463ee7583727cfb569306f8b
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-11-23 14:12:55 +0900

    fix: add EIP1559 gas fee data

--- src/domain/transaction/GasService.type.ts
+++ src/domain/transaction/GasService.type.ts
@@ -1,9 +1,17 @@
+import { BigNumber } from 'ethers';
+
 import { GAS_LEVEL } from '@@constants/transaction.constant';
 
 import { IGasFeeInfo, INetworkInfo } from './TransactionService.type';
-
+export interface IEIP1559GasFeeInfo {
+  maxFeePerGas: BigNumber | null;
+  maxPriorityFeePerGas: BigNumber | null;
+  gasPrice: BigNumber | null;
+  gasLimit: BigNumber;
+}
 export interface IGasService {
   getGasFeeData: (networkInfo: INetworkInfo) => Promise<IGasFeeInfo>;
+  getEIP1559GasFeeData: (networkInfo: INetworkInfo) => Promise<IEIP1559GasFeeInfo>;
 }
 
 export type TGasLevel = typeof GAS_LEVEL[keyof typeof GAS_LEVEL];
