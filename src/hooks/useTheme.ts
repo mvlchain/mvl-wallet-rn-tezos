ff91814980c04a3a58367fc0db8a6467effde076
cudymvl <cudy.kim@mvlchain.io>
2022-11-28 16:17:47 +0900

    feat: color hook and mnemonic

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,6 +1,9 @@
+import { useMemo } from 'react';
+
 import { SvgProps } from 'react-native-svg/lib/typescript/elements/Svg';
 
 import settingPersistStore from '@@store/setting/settingPersistStore';
+import { theme } from '@@style/theme';
 
 export const getAssetFromTheme = (appTheme: string, light: React.FC<SvgProps>, dark: React.FC<SvgProps>) => {
   let asset = light;
@@ -22,4 +25,12 @@ export const useAssetFromTheme = (light: any, dark: any) => {
   const asset = getAssetFromTheme(appTheme.value, light, dark);
 
   return asset;
+};
+
+export const useColor = () => {
+  const { appTheme } = settingPersistStore();
+  const color = useMemo(() => theme[appTheme.value].color, [appTheme.value]);
+  return {
+    color,
+  };
 };
