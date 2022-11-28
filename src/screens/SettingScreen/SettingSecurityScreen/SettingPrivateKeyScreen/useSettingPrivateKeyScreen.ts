84c68934774827c17a3934121d166b996ac259ce
cudymvl <cudy.kim@mvlchain.io>
2022-11-28 12:18:00 +0900

    fix: bottom select modal에 Key값 추가

--- src/screens/SettingScreen/SettingSecurityScreen/SettingPrivateKeyScreen/useSettingPrivateKeyScreen.ts
+++ src/screens/SettingScreen/SettingSecurityScreen/SettingPrivateKeyScreen/useSettingPrivateKeyScreen.ts
@@ -4,21 +4,20 @@ import Clipboard from '@react-native-clipboard/clipboard';
 import { useTranslation } from 'react-i18next';
 import Toast from 'react-native-toast-message';
 
-import { IBottomSelectMenuProps } from '@@components/BasicComponents/Modals/BottomSelectModal/BottomSelectMenu/BottomSelectMenu.type';
 import { MODAL_TYPES } from '@@components/BasicComponents/Modals/GlobalModal';
+import useWalletListModal from '@@components/BasicComponents/Modals/WalletListModal/useWalletListModal';
 import TOAST_DEFAULT_OPTION from '@@constants/toastConfig.constant';
 import { useDi } from '@@hooks/useDi';
 import globalModalStore from '@@store/globalModal/globalModalStore';
 
 const useSettingPrivateKeyScreen = () => {
   // TODO: 실제 데이터 연동
-  const pkey = 'testPkey!!';
-  const wallet_length = 2;
+  const [pkey, setPkey] = useState('');
   const { t } = useTranslation();
   const { openModal } = globalModalStore();
   const uiService = useDi('UIService');
   const [type, setType] = useState<'hide' | 'show'>('hide');
-  const [sampleWallet, setSampleWallet] = useState<string>('wallet1');
+  const { wallet } = useWalletListModal();
 
   const onPressViewPrivatekey = async () => {
     await uiService.triggerGetPincode();
@@ -37,35 +36,13 @@ const useSettingPrivateKeyScreen = () => {
   };
 
   const openWalletListModal = () => {
-    // TODO: 실제 데이터 연동
-    const dummyWalletList: IBottomSelectMenuProps[] = [
-      {
-        title: 'wallet 1',
-        isSelected: sampleWallet === 'wallet1',
-        onPress: () => {
-          setSampleWallet('wallet1');
-        },
-      },
-      {
-        title: 'wallet 2',
-        isSelected: sampleWallet === 'wallet2',
-        onPress: () => {
-          setSampleWallet('wallet2');
-        },
-      },
-      {
-        title: 'wallet 3',
-        isSelected: sampleWallet === 'wallet3',
-        onPress: () => {
-          setSampleWallet('wallet3');
-        },
-      },
-    ];
-    openModal(MODAL_TYPES.BOTTOM_SELECT, { modalTitle: t('select_private_key_title'), menuList: dummyWalletList });
+    if (!wallet) return;
+    openModal(MODAL_TYPES.WALLET_LIST, { menuList: wallet });
   };
 
   const onPressWalletList: Function | undefined = () => {
-    if (wallet_length > 1) {
+    if (!wallet) return;
+    if (wallet.length > 1) {
       return openWalletListModal;
     } else {
       return undefined;
@@ -75,7 +52,7 @@ const useSettingPrivateKeyScreen = () => {
   return {
     type,
     pkey,
-    wallet_length,
+    wallet,
     onPressViewPrivatekey,
     onPressCopyPrivateKey,
     onPressWalletList: onPressWalletList(),
