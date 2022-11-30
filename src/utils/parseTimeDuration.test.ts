96c2badfb5e47dcac3e18ed537255fafb31386cb
Darby <cutehackers@gmail.com>
2022-11-30 17:37:33 +0900

    feat: updated parseTimeDuration having current param as default parameter

--- src/utils/parseTimeDuration.test.ts
+++ src/utils/parseTimeDuration.test.ts
@@ -5,7 +5,7 @@ describe('TimeDuration', () => {
   it('days duration', () => {
     const current = new Date('2022-11-10T23:00:00.000Z');
     const target = new Date('2022-11-12T23:00:00.000Z');
-    const duration = parseTimeDuration(current, target);
+    const duration = parseTimeDuration(target, current);
 
     expect(duration?.amount ?? 0).toBe(2);
     expect(duration?.unit).toBe(TimeDurationUnit.Day);
@@ -14,7 +14,7 @@ describe('TimeDuration', () => {
   it('hours duration', () => {
     const current = new Date('2022-11-12T20:00:00.000Z');
     const target = new Date('2022-11-12T23:00:00.000Z');
-    const duration = parseTimeDuration(current, target);
+    const duration = parseTimeDuration(target, current);
 
     expect(duration?.amount ?? 0).toBe(3);
     expect(duration?.unit).toBe(TimeDurationUnit.Hour);
@@ -23,7 +23,7 @@ describe('TimeDuration', () => {
   it('minutes duration', () => {
     const current = new Date('2022-11-12T23:00:00.000Z');
     const target = new Date('2022-11-12T23:20:00.000Z');
-    const duration = parseTimeDuration(current, target);
+    const duration = parseTimeDuration(target, current);
 
     expect(duration?.amount ?? 0).toBe(20);
     expect(duration?.unit).toBe(TimeDurationUnit.Minute);
@@ -32,7 +32,7 @@ describe('TimeDuration', () => {
   it('zero duration with a value less then a minute', () => {
     const current = new Date('2022-11-12T23:00:00.000Z');
     const target = new Date('2022-11-12T23:00:20.000Z');
-    const duration = parseTimeDuration(current, target);
+    const duration = parseTimeDuration(target, current);
 
     expect(duration).toBeNull();
   });
@@ -40,7 +40,7 @@ describe('TimeDuration', () => {
   it('zero duration with equal time', () => {
     const current = new Date('2022-11-12T23:00:00.000Z');
     const target = new Date('2022-11-12T23:00:00.000Z');
-    const duration = parseTimeDuration(current, target);
+    const duration = parseTimeDuration(target, current);
 
     expect(duration).toBeNull();
   });
