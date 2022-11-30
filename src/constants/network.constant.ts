3b7cc114b067242ffa5d3d9d9971d61c7f1b453b
gin1231 <jineok12@gmail.com>
2022-11-30 16:56:48 +0900

    apply new network constant structure

--- src/constants/network.constant.ts
+++ src/constants/network.constant.ts
@@ -2,18 +2,81 @@ import { valueOf } from '@@utils/types';
 
 export const NETWORK = {
   ETH: 'ETHEREUM',
+  GOERLI: 'GOERLI',
   BSC: 'BSC',
+  BSC_TESTNET: 'BSC_TESTNET',
+  TEZOS: 'TEZOS',
+  TEZOS_GHOSTNET: 'TEZOS_GHOSTNET',
 } as const;
 
 export type Network = valueOf<typeof NETWORK>;
 
-export const NETWORK_STRINGS: Record<string, Record<Network | string, string>> = {
-  mainnet: {
-    [NETWORK.ETH]: 'Ethereum Mainnet',
-    [NETWORK.BSC]: 'Binance Smart Chain',
-  },
-  testnet: {
-    [NETWORK.ETH]: 'Ethereum Testnet',
-    [NETWORK.BSC]: 'Binance Smart Chain Testnet',
-  },
+export const NETWORK_FEE_TYPE = {
+  EVM_LEGACY_GAS: 'EVM_LEGACY_GAS',
+  EIP1559: 'EIP1559',
+  TEZOS: 'TEZOS',
 } as const;
+
+export type NetworkFeeType = valueOf<typeof NETWORK_FEE_TYPE>;
+
+export type NetworkConfig = {
+  name: string;
+  shortName: string;
+  chainId: number;
+  bip44: number;
+  rpcUrl: string;
+  networkFeeType: NetworkFeeType;
+};
+
+export const NETWORK_CONFIGS: Record<Network, NetworkConfig> = {
+  [NETWORK.ETH]: {
+    name: 'Ethereum Main Network',
+    shortName: 'Ethereum',
+    chainId: 1,
+    bip44: 60,
+    rpcUrl: '***REMOVED***',
+    networkFeeType: NETWORK_FEE_TYPE.EIP1559,
+  },
+  [NETWORK.GOERLI]: {
+    name: 'Ethereum Goerli Testnet',
+    shortName: 'Goerli Testnet',
+    chainId: 5,
+    bip44: 60,
+    rpcUrl: 'https://goerli.infura.io/v3/***REMOVED***',
+    networkFeeType: NETWORK_FEE_TYPE.EIP1559,
+  },
+  [NETWORK.BSC]: {
+    name: 'BNB Smart Chain',
+    shortName: 'BSC',
+    chainId: 56,
+    bip44: 60,
+    rpcUrl: '***REMOVED***',
+    networkFeeType: NETWORK_FEE_TYPE.EVM_LEGACY_GAS,
+  },
+  [NETWORK.BSC_TESTNET]: {
+    name: 'BNB Smart Chain - Testnet',
+    shortName: 'BSC Testnet',
+    chainId: 97,
+    bip44: 60,
+    rpcUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    networkFeeType: NETWORK_FEE_TYPE.EVM_LEGACY_GAS,
+  },
+  [NETWORK.TEZOS]: {
+    name: 'Tezos Mainnet',
+    shortName: 'Tezos',
+    chainId: 1729,
+    bip44: 1729,
+    rpcUrl: 'https://mainnet-node.madfish.solutions/',
+    networkFeeType: NETWORK_FEE_TYPE.TEZOS,
+  },
+  [NETWORK.TEZOS_GHOSTNET]: {
+    name: 'Tezos Ghostnet',
+    shortName: 'Tezos Ghostnet',
+    chainId: 1729,
+    bip44: 1729,
+    rpcUrl: 'https://ghostnet.smartpy.io/',
+    networkFeeType: NETWORK_FEE_TYPE.TEZOS,
+  },
+};
+
+export const getNetworkConfig = (network: Network): NetworkConfig => NETWORK_CONFIGS[network];
