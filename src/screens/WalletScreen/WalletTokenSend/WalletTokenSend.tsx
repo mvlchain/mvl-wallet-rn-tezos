ebaf0cf63b5b9179546c7e88c72fa6ee6217cf78
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-12-05 17:24:56 +0900

    fix: 네비게이션 옵션 수정

--- src/screens/WalletScreen/WalletTokenSend/WalletTokenSend.tsx
+++ src/screens/WalletScreen/WalletTokenSend/WalletTokenSend.tsx
@@ -1,36 +1,27 @@
-import React, { useLayoutEffect, useState } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
 
-import { useRoute, useNavigation } from '@react-navigation/native';
-import { useTranslation } from 'react-i18next';
-import { Text, View } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 import { ScrollView } from 'react-native-gesture-handler';
-import { add } from 'react-native-reanimated';
 
 import Divider from '@@components/BasicComponents/Divider';
 import { DIVIDER_THICKNESS } from '@@components/BasicComponents/Divider/Divider.type';
 import GasFeeBoard from '@@components/BasicComponents/GasFeeBoard';
 import SendInputBoard from '@@components/WalletTokenSend/SendInputBoard';
-import useHeader from '@@hooks/useHeader';
 
-import { TTokenSendRootStackProps, TTokenSendRouteProps } from './WalletTokenSend.type';
+import { TTokenSendRouteProps } from './WalletTokenSend.type';
 import useTokenSend from './useTokenSend';
 
 function WalletTokenSend() {
   const { params } = useRoute<TTokenSendRouteProps>();
-  const { handleStackHeaderOption } = useHeader();
-  const navigation = useNavigation<TTokenSendRootStackProps>();
-  const { t } = useTranslation();
 
-  useLayoutEffect(() => {
-    const headerOption = handleStackHeaderOption({ title: t('send') });
-    navigation.setOptions(headerOption);
-  }, []);
-  const { amount, setAmount, address, setAddress, confirmSend } = useTokenSend(params.tokenDto);
+  const { amount, setAmount, address, setAddress, confirm } = useTokenSend(params.tokenDto);
   return (
+    // eslint-disable-next-line react-native/no-inline-styles
     <ScrollView style={{ flex: 1 }}>
       <SendInputBoard amount={amount} setAmount={setAmount} address={address} setAddress={setAddress} />
       <Divider thickness={DIVIDER_THICKNESS.THICK} />
-      <GasFeeBoard isRevision={false} onConfirm={confirmSend} tokenDto={params.tokenDto} />
+      <GasFeeBoard isRevision={false} onConfirm={confirm} tokenDto={params.tokenDto} />
     </ScrollView>
   );
 }
--- src/screens/WalletScreen/WalletTransactionHistory/WalletTransactionHistory.tsx
+++ src/screens/WalletScreen/WalletTransactionHistory/WalletTransactionHistory.tsx
@@ -1,9 +1,6 @@
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 
-import { useRoute, RouteProp, useNavigation } from '@react-navigation/native';
-import { Transaction } from 'bitcoinjs-lib';
-import { Trans, useTranslation } from 'react-i18next';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import Divider from '@@components/BasicComponents/Divider';
 import { DIVIDER_THICKNESS } from '@@components/BasicComponents/Divider/Divider.type';
@@ -11,22 +8,10 @@ import PendingTransactionButtons from '@@components/WalletTransactionHistory/Pen
 import TransactionDetailBoard from '@@components/WalletTransactionHistory/TransactionDetailBoard';
 import TransactionFeeBoard from '@@components/WalletTransactionHistory/TransactionFeeBoard';
 import TransactionHashBoard from '@@components/WalletTransactionHistory/TransactionHashBoard';
-import useHeader from '@@hooks/useHeader';
 
 import * as S from './WalletTransactionHistory.style';
-import { TTransactionHistoryRouteProps, TTransactionHistoryRootStackProps } from './WalletTransactionHistory.type';
 
 function WalletTransactionHistory() {
-  const { params } = useRoute<TTransactionHistoryRouteProps>();
-  const { handleStackHeaderOption } = useHeader();
-  const navigation = useNavigation<TTransactionHistoryRootStackProps>();
-  const { t } = useTranslation();
-
-  useLayoutEffect(() => {
-    const headerOption = handleStackHeaderOption({ title: t('transaction_history') });
-    navigation.setOptions(headerOption);
-  }, []);
-
   return (
     <S.Container>
       <View>
