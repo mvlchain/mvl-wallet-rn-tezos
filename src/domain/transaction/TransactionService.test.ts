3c9288f257d0d2fd8a93c9184938c3ad88b78406
cudymvl <cudy.kim@mvlchain.io>
2022-12-05 10:01:39 +0900

    fix: delete client file to fix name uppercase

--- src/domain/transaction/TransactionService.test.ts
+++ src/domain/transaction/TransactionService.test.ts
@@ -7,7 +7,7 @@ const transactionService: TransactionService = new EthersTransactionImpl(
   '0x0000000000000000000000000000000000000000000000000000000000000000',
   {
     gasPrice: ethers.utils.formatUnits(BigNumber.from('100'), 'gwei'),
-  },
+  }
 );
 
 it('constructs correctly', () => {
