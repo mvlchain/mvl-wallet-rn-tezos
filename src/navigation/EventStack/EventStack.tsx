89f91dab9832bc5a38b125bbfad1dbb08df56564
Darby <cutehackers@gmail.com>
2022-12-05 18:12:28 +0900

    feat: added top bar to EarnEventDetailsScreen

--- src/navigation/EventStack/EventStack.tsx
+++ src/navigation/EventStack/EventStack.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import useHeader from '@@hooks/useHeader';
 import { EarnEventListScreen, EarnEventDetailsScreen } from '@@screens/EarnEventScreen';
 
 import { TEventStackParamList, EVENT_STACK_ROUTE } from './EventStack.type';
 
-const { Navigator, Screen } = createNativeStackNavigator<TEventStackParamList>();
+const { Navigator, Screen } = createStackNavigator<TEventStackParamList>();
+
+type ScreenProps = Parameters<typeof Screen>[0];
 
 function EventStack() {
+  const { handleStackHeaderOption } = useHeader();
+
+  const screens: Array<ScreenProps> = [
+    {
+      name: EVENT_STACK_ROUTE.EVENT,
+      component: EarnEventListScreen,
+    },
+    {
+      name: EVENT_STACK_ROUTE.DETAILS,
+      component: EarnEventDetailsScreen,
+      options: {
+        ...handleStackHeaderOption({ title: '' }),
+        headerShown: true,
+      },
+    },
+  ];
+
   return (
     <Navigator
       initialRouteName={EVENT_STACK_ROUTE.EVENT}
       screenOptions={{
         headerShown: false,
+        headerTitleAlign: 'left',
       }}
     >
-      <Screen key={EVENT_STACK_ROUTE.EVENT} name={EVENT_STACK_ROUTE.EVENT} component={EarnEventListScreen} />
-      <Screen key={EVENT_STACK_ROUTE.DETAILS} name={EVENT_STACK_ROUTE.DETAILS} component={EarnEventDetailsScreen} />
+      {screens.map((props) => (
+        <Screen key={props.name} {...props} />
+      ))}
     </Navigator>
   );
 }
