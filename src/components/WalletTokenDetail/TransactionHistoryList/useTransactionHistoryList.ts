fd55c65b91b7a2992a75b2b69aac4fa21a75f445
gin1231 <jineok12@gmail.com>
2022-12-11 21:57:48 +0900

    query transaction history only when myAddress exists

--- src/components/WalletTokenDetail/TransactionHistoryList/useTransactionHistoryList.ts
+++ src/components/WalletTokenDetail/TransactionHistoryList/useTransactionHistoryList.ts
@@ -5,7 +5,7 @@ import { useRoute } from '@react-navigation/native';
 import { getNetworkConfig, getNetworkName } from '@@constants/network.constant';
 import { TRANSACTION_HISTORY_FILTER_CRITERIA } from '@@constants/transaction.constant';
 import { IGetTransactionHistoryResponse } from '@@domain/transaction/TransactionService.type';
-import useTransactionHitoryQuery from '@@hooks/queries/useTransactionHistoryQuery';
+import useTransactionHistoryQuery from '@@hooks/queries/useTransactionHistoryQuery';
 import { useDi } from '@@hooks/useDi';
 import { TTokenDetailRouteProps } from '@@screens/WalletScreen/WalletTokenDetail/WalletTokenDetail.type';
 import walletPersistStore from '@@store/wallet/walletPersistStore';
@@ -23,7 +23,7 @@ const useTransactionHistoryList = () => {
   const [beforeindex, setBeforeIndex] = useState(2147483647);
   const { currentCriteria, filterCriteria } = useTransactionHistoryFilter();
 
-  const [myaddress, setMyAddress] = useState('');
+  const [myAddress, setMyAddress] = useState('');
   const setWallet = async () => {
     const wallet = await walletService.getWalletInfo({ index: selectedWalletIndex[selectedNetwork], network: selectedNetwork });
     setMyAddress(wallet.address);
@@ -33,21 +33,21 @@ const useTransactionHistoryList = () => {
   }, []);
 
   const filteredData = useMemo(() => {
-    if (!history || !myaddress) return;
+    if (!history || !myAddress) return;
     switch (currentCriteria) {
       case TRANSACTION_HISTORY_FILTER_CRITERIA.SENT_ONLY:
-        return history.filter((v, i) => v.from === myaddress);
+        return history.filter((v, i) => v.from === myAddress);
       case TRANSACTION_HISTORY_FILTER_CRITERIA.RECEIVED_ONLY:
-        return history.filter((v, i) => v.to === myaddress);
+        return history.filter((v, i) => v.to === myAddress);
       default:
         return history;
     }
-  }, [currentCriteria, history.length, myaddress]);
+  }, [currentCriteria, history.length, myAddress]);
 
-  const { refetch } = useTransactionHitoryQuery(
+  const { refetch } = useTransactionHistoryQuery(
     {
       network: pickNetwork,
-      address: myaddress,
+      address: myAddress,
       ticker: params.tokenDto.symbol,
       beforeblock,
       beforeindex,
@@ -62,6 +62,7 @@ const useTransactionHistoryList = () => {
         }
       },
       keepPreviousData: true,
+      enabled: !!myAddress,
     }
   );
 
