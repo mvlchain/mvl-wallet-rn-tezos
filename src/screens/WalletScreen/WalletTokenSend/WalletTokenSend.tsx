9023fb5bc4548e5efffb676b1455823a368fe037
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-12-12 19:58:49 +0900

    keyboard dismiss add

--- src/screens/WalletScreen/WalletTokenSend/WalletTokenSend.tsx
+++ src/screens/WalletScreen/WalletTokenSend/WalletTokenSend.tsx
@@ -2,7 +2,8 @@
 import React from 'react';
 
 import { useRoute } from '@react-navigation/native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Keyboard } from 'react-native';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 import Divider from '@@components/BasicComponents/Divider';
 import { DIVIDER_THICKNESS } from '@@components/BasicComponents/Divider/Divider.type';
@@ -19,9 +20,11 @@ function WalletTokenSend() {
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <ScrollView style={{ flex: 1 }}>
-      <SendInputBoard amount={amount} setAmount={setAmount} address={address} setAddress={setAddress} />
-      <Divider thickness={DIVIDER_THICKNESS.THICK} />
-      <GasFeeBoard isRevision={false} onConfirm={confirm} tokenDto={params.tokenDto} />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <SendInputBoard amount={amount} setAmount={setAmount} address={address} setAddress={setAddress} />
+        <Divider thickness={DIVIDER_THICKNESS.THICK} />
+        <GasFeeBoard isRevision={false} onConfirm={confirm} tokenDto={params.tokenDto} />
+      </TouchableWithoutFeedback>
     </ScrollView>
   );
 }
