af1f686450ec0405dc6fe73c1e4293567b5b1811
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-12-18 16:23:39 +0900

    addresstextfield add

---
+++ src/components/BasicComponents/TextFields/AddressTextField/AddressTextField.type.ts
@@ -0,0 +1,7 @@
+export interface IAddressTextFieldProps {
+  value: any;
+  onChange: (value: any) => void;
+  placeholder?: string;
+  label?: string;
+  gotoScan?: Function;
+}
--- src/components/WalletTokenSend/SendInputBoard.tsx
+++ src/components/WalletTokenSend/SendInputBoard.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { useTranslation } from 'react-i18next';
 
-import { BaseTextField } from '@@components/BasicComponents/TextFields/BaseTextField';
-import { TradeVolume } from '@@components/BasicComponents/TextFields/TradeVolume';
+import AddressTextField from '@@components/BasicComponents/TextFields/AddressTextField';
+import { TradeVolume } from '@@components/BasicComponents/TextFields/TradeVolume/TradeVolume';
 import { ROOT_STACK_ROUTE } from '@@navigation/RootStack/RootStack.type';
 import { TScanQRRootStackProps } from '@@screens/WalletScreen/WalletScanQR/WalletScanQR.type';
 import { TTokenSendRouteProps } from '@@screens/WalletScreen/WalletTokenSend/WalletTokenSend.type';
@@ -22,15 +22,7 @@ function SendInputBoard({ amount, setAmount, address, setAddress }: ISendInputBo
 
   return (
     <S.Container>
-      <BaseTextField
-        value={address}
-        onChange={setAddress}
-        scanable={true}
-        type={'address'}
-        label={t('send_address')}
-        placeholder={t('input_address')}
-        gotoScan={gotoScan}
-      />
+      <AddressTextField value={address} onChange={setAddress} label={t('send_address')} placeholder={t('input_address')} gotoScan={gotoScan} />
       <S.Gap />
       <TradeVolume label={t('send_amount')} onChange={setAmount} value={amount} useMax={true} tokenDto={params.tokenDto} />
     </S.Container>
