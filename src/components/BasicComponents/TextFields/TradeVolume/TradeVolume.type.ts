baedea11d3956b4aae4b5f988fe4e2f72c7c64d7
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-12-19 01:22:49 +0900

    bug fix

--- src/components/BasicComponents/TextFields/TradeVolume/TradeVolume.type.ts
+++ src/components/BasicComponents/TextFields/TradeVolume/TradeVolume.type.ts
@@ -12,5 +12,5 @@ export interface ITradeVolumeComponentProps {
   iconUrl?: string;
   disableHint?: boolean;
   debounceTime?: number;
-  setParentValid: (valid: boolean) => void;
+  setParentValid?: (valid: boolean) => void;
 }
