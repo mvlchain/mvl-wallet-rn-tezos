4314d161ffb03f7215757644fa34479e194323e6
choichoikule <67899064+choichoikule@users.noreply.github.com>
2022-12-19 15:46:21 +0900

    confirm tx 모달 전역스토어값 받아오도록

--- src/components/BasicComponents/Modals/Transaction/ConfirmSendModal/ConfirmSendModal.tsx
+++ src/components/BasicComponents/Modals/Transaction/ConfirmSendModal/ConfirmSendModal.tsx
@@ -9,8 +9,10 @@ import { DIVIDER_THICKNESS } from '@@components/BasicComponents/Divider/Divider.
 import { ModalLayout } from '@@components/BasicComponents/Modals/BaseModal/ModalLayout';
 import { COIN_DTO, getNetworkConfig, getNetworkName, NETWORK_FEE_TYPE } from '@@constants/network.constant';
 import useOneTokenPrice from '@@hooks/useOneTokenPrice';
+import gasStore from '@@store/gas/gasStore';
 import globalModalStore from '@@store/globalModal/globalModalStore';
 import settingPersistStore from '@@store/setting/settingPersistStore';
+import { transactionRequestStore } from '@@store/transaction/transactionRequestStore';
 import walletPersistStore from '@@store/wallet/walletPersistStore';
 import { formatBigNumber } from '@@utils/formatBigNumber';
 import { height } from '@@utils/ui';
@@ -20,7 +22,7 @@ import { MODAL_TYPES } from '../../GlobalModal';
 import * as S from './ConfirmSendModal.style';
 import { IConfirmSendModalProps } from './ConfirmSendModal.type';
 
-function ConfirmSendModal({ recipientAddress, amount, fee, onConfirm, tokenDto }: IConfirmSendModalProps) {
+function ConfirmSendModal({ onConfirm, tokenDto }: IConfirmSendModalProps) {
   const { t } = useTranslation();
   const { modalType, closeModal } = globalModalStore();
   const { selectedNetwork: pickNetwork } = walletPersistStore();
@@ -28,9 +30,13 @@ function ConfirmSendModal({ recipientAddress, amount, fee, onConfirm, tokenDto }
   const network = getNetworkConfig(selectedNetwork);
   const { settedCurrency } = settingPersistStore();
 
-  const amountStr = formatBigNumber(amount, tokenDto.decimals).toString(10);
+  const { total } = gasStore();
+  const { value, to } = transactionRequestStore();
+
+  const totalStr = formatBigNumber(total!, tokenDto.decimals).toString(10);
+  const amountStr = formatBigNumber(value!, tokenDto.decimals).toString(10);
   const { price: tokenPrice } = useOneTokenPrice(tokenDto, amountStr);
-  const { price: coinPrice } = useOneTokenPrice(COIN_DTO[network.coin], fee);
+  const { price: coinPrice } = useOneTokenPrice(COIN_DTO[network.coin], totalStr);
   const tokenPriceInDeciaml = new Decimal(tokenPrice);
   const coinPriceInDecimal = new Decimal(coinPrice);
 
@@ -51,20 +57,20 @@ function ConfirmSendModal({ recipientAddress, amount, fee, onConfirm, tokenDto }
     >
       <S.TopContainer>
         <S.GreyText>{'Recipient Address'}</S.GreyText>
-        <S.LargeBlackText>{recipientAddress}</S.LargeBlackText>
+        <S.LargeBlackText>{to}</S.LargeBlackText>
       </S.TopContainer>
       <S.MiddleContainer>
         <S.Row>
           <S.BlackText> {t('send_amount')}</S.BlackText>
           <S.RightAlign>
-            <S.BlackText>{amount && `${amountStr} ${tokenDto.symbol}`}</S.BlackText>
+            <S.BlackText>{value && `${amountStr} ${tokenDto.symbol}`}</S.BlackText>
             <S.GreyText>{`${tokenPrice} ${settedCurrency}`}</S.GreyText>
           </S.RightAlign>
         </S.Row>
         <S.Row style={{ marginBottom: height * 16 }}>
           <S.BlackText>{t('transaction_fee')}</S.BlackText>
           <S.RightAlign>
-            <S.BlackText>{fee && `${fee} ${network.coin}`}</S.BlackText>
+            <S.BlackText>{totalStr && `${totalStr} ${network.coin}`}</S.BlackText>
             <S.GreyText>{`${coinPrice} ${settedCurrency}`}</S.GreyText>
           </S.RightAlign>
         </S.Row>
@@ -72,7 +78,7 @@ function ConfirmSendModal({ recipientAddress, amount, fee, onConfirm, tokenDto }
         <S.Row>
           <S.BlackText>{t('total')}</S.BlackText>
           <S.RightAlign>
-            <S.BlackText>{amount && fee && `${amountStr} ${tokenDto.symbol} + ${fee} ${network.coin}`}</S.BlackText>
+            <S.BlackText>{value && totalStr && `${amountStr} ${tokenDto.symbol} + ${totalStr} ${network.coin}`}</S.BlackText>
             <S.GreyText>{`${feeAmountTotal} ${settedCurrency}`}</S.GreyText>
           </S.RightAlign>
         </S.Row>
