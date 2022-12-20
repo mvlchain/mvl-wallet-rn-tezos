2bafeab220413dd8ff1067b18461c0c60db4f4c4
cudymvl <cudy.kim@mvlchain.io>
2022-12-20 21:57:35 +0900

    feat: test onRefresh

--- src/screens/WalletScreen/WalletTransactionHistory/WalletTransactionHistory.style.ts
+++ src/screens/WalletScreen/WalletTransactionHistory/WalletTransactionHistory.style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   flex: 1;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   background-color: ${({ theme }) => theme.color.whiteBlack};
 `;
