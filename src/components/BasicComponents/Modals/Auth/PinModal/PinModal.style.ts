1fa58d20baf3b2aa1e7a73293d944d39737fd95e
choichoikule <67899064+choichoikule@users.noreply.github.com>
2023-01-02 17:07:33 +0900

    auth pin modal loading indicator add and style fix

---
+++ src/components/BasicComponents/Modals/Auth/PinModal/PinModal.style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+export const ModalContentWrapper = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.color.whiteBlack};
+`;
