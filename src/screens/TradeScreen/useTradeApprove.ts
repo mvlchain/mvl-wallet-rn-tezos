bd7507f98ae11ccbcccba567f6319c77519bff9c
choichoikule <67899064+choichoikule@users.noreply.github.com>
2023-01-05 17:57:02 +0900

    trade approve init

---
+++ src/screens/TradeScreen/useTradeApprove.ts
@@ -0,0 +1,13 @@
+const useTradeApprove = () => {
+  //spender 물어보기 쿼리 만들기
+  //allowance확인
+  //allowance비교
+  //gasfeesetting
+  //approve요청
+
+  const approve = () => {};
+
+  return { approve };
+};
+
+export default useTradeApprove;
