5a2b9d4f1ea748fedbc1d5a4e979aaaf29e4cbb5
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2023-01-06 22:20:49 +0900

    useSpender add

--- src/screens/TradeScreen/useTradeApprove.ts
+++ src/screens/TradeScreen/useTradeApprove.ts
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
+import { getNetworkConfig } from '@@constants/network.constant';
+import useSpender from '@@hooks/queries/useSpender';
+import walletPersistStore from '@@store/wallet/walletPersistStore';
 const useTradeApprove = () => {
   //spender 물어보기 쿼리 만들기
   //allowance확인
   //allowance비교
   //gasfeesetting
   //approve요청
+  const [spenders, setSpenders] = useState<string[]>();
+  const { selectedNetwork } = walletPersistStore();
+  const { data } = useSpender(getNetworkConfig(selectedNetwork).networkId, {
+    onSuccess: (data) => {
+      setSpenders(data.map((spender) => spender.address));
+    },
+  });
 
   const approve = () => {};
 
