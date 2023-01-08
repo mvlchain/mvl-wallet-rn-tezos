e7490d7eebf0a96905ba84556aaec47275d51851
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2023-01-08 20:09:41 +0900

    delete unuse method

--- src/domain/token/repositories/TokenRepository.ts
+++ src/domain/token/repositories/TokenRepository.ts
@@ -5,19 +5,22 @@ import { inject, injectable } from 'tsyringe';
 import { abiERC20 } from '@@constants/contract/abi/abiERC20';
 import { NetworkId, getNetworkConfig, Network } from '@@constants/network.constant';
 import { EvmJsonRpcProviderHolder } from '@@domain/blockchain/EvmJsonRpcProviderHolder';
+import { WalletService } from '@@domain/wallet/services/WalletService';
 import { TokenDto } from '@@store/token/tokenPersistStore.type';
-import { BnToEtherBn, etherBNtoBN } from '@@utils/formatBigNumber';
+import { etherBNtoBN } from '@@utils/formatBigNumber';
 import { request } from '@@utils/request';
 
 export interface ITokenRepository {
   getToken: (network: NetworkId) => Promise<TokenDto[]>;
-  getAllowance: (selectedNetwork: Network, selectedWalletPrivateKey: string, spender: string, contractAddress: string) => Promise<BigNumber | null>;
-  setApprove: (selectedNetwork: Network, spender: string, contractAddress: string, value: BigNumber) => Promise<boolean>;
+  getAllowance: (selectedNetwork: Network, selectedWalletIndex: number, spender: string, contractAddress: string) => Promise<BigNumber | null>;
 }
 
 @injectable()
 export class TokenRepository implements ITokenRepository {
-  constructor(@inject('EvmJsonRpcProviderHolder') private evmJsonRpcProviderHolder: EvmJsonRpcProviderHolder) {}
+  constructor(
+    @inject('EvmJsonRpcProviderHolder') private evmJsonRpcProviderHolder: EvmJsonRpcProviderHolder,
+    @inject('WalletService') private walletService: WalletService
+  ) {}
 
   getToken = async (network: NetworkId): Promise<TokenDto[]> => {
     const endpoint = `v1/token/${network}`;
@@ -25,21 +28,13 @@ export class TokenRepository implements ITokenRepository {
     return res.data;
   };
 
-  getAllowance = async (selectedNetwork: Network, selectedWalletPrivateKey: string, spender: string, contractAddress: string) => {
+  getAllowance = async (selectedNetwork: Network, selectedWalletIndex: number, spender: string, contractAddress: string) => {
     const network = getNetworkConfig(selectedNetwork);
     const provider = this.evmJsonRpcProviderHolder.getProvider(network.rpcUrl);
-    const wallet = new ethers.Wallet(selectedWalletPrivateKey, provider);
+    const wallet = await this.walletService.getWalletInfo({ index: selectedWalletIndex, network: selectedNetwork });
     const abi = abiERC20;
     const contract = new ethers.Contract(contractAddress, abi, provider);
     const allowance = await contract.allowance(wallet.address, spender);
     return etherBNtoBN(allowance);
-  };
-
-  setApprove = async (selectedNetwork: Network, spender: string, contractAddress: string, value: BigNumber) => {
-    const network = getNetworkConfig(selectedNetwork);
-    const provider = this.evmJsonRpcProviderHolder.getProvider(network.rpcUrl);
-    const abi = abiERC20;
-    const contract = new ethers.Contract(contractAddress, abi, provider);
-    return await contract.approve(spender, BnToEtherBn(value));
   };
 }
