11a13af44cabf938461d4c773fc1c3ffbd081b72
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2023-01-08 20:11:46 +0900

    useTradeApprove 작성

--- src/screens/TradeScreen/useTradeApprove.ts
+++ src/screens/TradeScreen/useTradeApprove.ts
@@ -1,25 +1,73 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+import BigNumber from 'bignumber.js';
+
+import { MODAL_TYPES } from '@@components/BasicComponents/Modals/GlobalModal';
 import { getNetworkConfig } from '@@constants/network.constant';
+import { IGasFeeInfo } from '@@domain/gas/GasService.type';
+import { TokenDto } from '@@generated/generated-scheme-clutch';
 import useSpender from '@@hooks/queries/useSpender';
+import { useDi } from '@@hooks/useDi';
+import globalModalStore from '@@store/globalModal/globalModalStore';
+import tradeStore from '@@store/trade/tradeStore';
+import { transactionRequestStore } from '@@store/transaction/transactionRequestStore';
 import walletPersistStore from '@@store/wallet/walletPersistStore';
-const useTradeApprove = () => {
-  //spender 물어보기 쿼리 만들기
-  //allowance확인
-  //allowance비교
-  //gasfeesetting
-  //approve요청
-  const [spenders, setSpenders] = useState<string[]>();
-  const { selectedNetwork } = walletPersistStore();
+
+//spender 물어보기
+//allowance확인
+//allowance비교
+//gasfeesetting
+//approve요청
+const useTradeApprove = (fromToken: TokenDto) => {
+  const TokenRepository = useDi('TokenRepository');
+  const TransactionService = useDi('TransactionService');
+  const [spender, setSpender] = useState<string>();
+  const [allowance, setAllowance] = useState<BigNumber | null>(null);
+  const { openModal, closeModal } = globalModalStore();
+  const { selectedNetwork, selectedWalletIndex } = walletPersistStore();
+  const { selectedToken, selectToken } = tradeStore();
+  const { to, value, data: approveData, setState } = transactionRequestStore();
+  const WBNB_ADDRESS = '';
+
   const { data } = useSpender(getNetworkConfig(selectedNetwork).networkId, {
     onSuccess: (data) => {
-      setSpenders(data.map((spender) => spender.address));
+      setSpender(data[0].address);
     },
   });
 
-  const approve = () => {};
+  useEffect(() => {
+    getAllowance();
+  }, [spender, selectedToken.from]);
 
-  return { approve };
+  const getAllowance = async () => {
+    if (!spender || !selectedToken.from) return;
+
+    const contractAddress = selectedToken.from === 'BNB' ? WBNB_ADDRESS : (fromToken.contractAddress as string);
+    const allowance = await TokenRepository.getAllowance(selectedNetwork, selectedWalletIndex[selectedNetwork], spender, contractAddress);
+    setAllowance(allowance);
+  };
+
+  const sendApproveTransaction = async (gasFeeInfo: IGasFeeInfo) => {
+    if (!to || !approveData) return;
+    const contractAddress = selectedToken.from === 'BNB' ? WBNB_ADDRESS : (fromToken.contractAddress as string);
+    await TransactionService.sendTransaction({
+      selectedNetwork,
+      selectedWalletIndex: selectedWalletIndex[selectedNetwork],
+      to: contractAddress,
+      data: approveData,
+      gasFeeInfo,
+    });
+    closeModal();
+  };
+
+  const onPressApprove = async () => {
+    if (!spender || !value) return;
+    const approveData = await TransactionService.getApproveData(spender, value);
+    setState({ data: approveData });
+    openModal(MODAL_TYPES.GAS_FEE, { tokenDto: fromToken, onConfirm: sendApproveTransaction });
+  };
+
+  return { allowance, onPressApprove };
 };
 
 export default useTradeApprove;
