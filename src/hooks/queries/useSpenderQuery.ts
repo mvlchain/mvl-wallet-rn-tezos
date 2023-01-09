1550c92b5cd71e1e668c0416bcc60e0a4d7bff29
choichoikule <67899064+choichoikule@users.noreply.github.com>
2023-01-09 12:35:13 +0900

    trade approve fix

--- src/hooks/queries/useSpenderQuery.ts
+++ src/hooks/queries/useSpenderQuery.ts
@@ -4,10 +4,10 @@ import { NetworkId } from '@@constants/network.constant';
 import { SpenderResponseDto } from '@@generated/generated-scheme-clutch';
 import { useDi } from '@@hooks/useDi';
 
-export default function useSpender(network: NetworkId, options: UseQueryOptions<SpenderResponseDto[], unknown, SpenderResponseDto[]> = {}) {
+export default function useSpenderQuery(network: NetworkId, options: UseQueryOptions<SpenderResponseDto[], unknown, SpenderResponseDto[]> = {}) {
   const TradeRepository = useDi('TradeRepository');
   return useQuery<SpenderResponseDto[], unknown, SpenderResponseDto[]>(createKey(network), () => TradeRepository.getSpender(network), options);
 }
 
 const createKey = (network: NetworkId) => ['spender', network];
-useSpender.createKey = createKey;
+useSpenderQuery.createKey = createKey;
