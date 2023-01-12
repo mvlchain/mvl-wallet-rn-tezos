fa1c56f1110b1214550b13edc46b989638235244
cutehackers <cutehackers@gmail.com>
2023-01-12 12:28:22 +0900

    feat: added test cases that checks wallet address (ethereum, tezos)

--- src/domain/auth/QrCodeParser/QrCodeParser.test.ts
+++ src/domain/auth/QrCodeParser/QrCodeParser.test.ts
@@ -1,6 +1,5 @@
+/* eslint-disable max-len */
 import { parse } from 'url';
-
-import { isAddress } from 'ethers/lib/utils';
 
 import QrCodeParser from './QrCodeParser';
 
@@ -26,29 +25,41 @@ test('QrCodeParser address stating with hex address', () => {
 
 test('QrCodeParser parsing path from url', () => {
   const url = parse(
-    'https://l.mvlclutch.io/link-transfer/ETHEREUM\
-    ?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
+    'https://l.mvlclutch.io/link-transfer/ETHEREUM?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
   );
   expect(url.path).toBe(
-    '/link-transfer/ETHEREUM\
-    ?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
+    '/link-transfer/ETHEREUM?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
   );
 });
 
 test('QrCodeParser parsing pathname from url', () => {
   const url = parse(
-    'https://l.mvlclutch.io/link-transfer/ETHEREUM\
-    ?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
+    'https://l.mvlclutch.io/link-transfer/ETHEREUM?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
   );
   expect(url.pathname).toBe('/link-transfer/ETHEREUM');
 });
 
 test('QrCodeParser parsing network from pathname', () => {
   const url = parse(
-    'https://l.mvlclutch.io/link-transfer/ETHEREUM\
-    ?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
+    'https://l.mvlclutch.io/link-transfer/ETHEREUM?tokenAddress=0x3e39300683cf271e067e22431f4c66bb0778a3eb&address=0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637&value=100000000000000000'
   );
   const paths = url.pathname?.split('/');
   expect(paths?.length).toBe(3);
   expect(paths?.[2]).toBe('ETHEREUM');
+});
+
+test('QrCodeParser valid ethereum address', () => {
+  expect(QrCodeParser.isWalletAddress('0x6CeE88A4298C937ac992Bf7c1806680f6c0d7637')).toBe(true);
+});
+
+test('QrCodeParser valid ethereum address(without hex prefix)', () => {
+  expect(QrCodeParser.isWalletAddress('6CeE88A4298C937ac992Bf7c1806680f6c0d7637')).toBe(true);
+});
+
+test('QrCodeParser valid tezos address', () => {
+  expect(QrCodeParser.isWalletAddress('tz1L9r8mWmRPndRhuvMCWESLGSVeFzQ9NAWx')).toBe(true);
+});
+
+test('QrCodeParser valid tezos address', () => {
+  expect(QrCodeParser.isWalletAddress('1L9r8mWmRPndRhuvMCWESLGSVeFzQ9NAWx')).toBe(false);
 });
