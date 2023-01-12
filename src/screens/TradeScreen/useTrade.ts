2898ffe70620102bde4cd674bb2d656631f41e4d
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2023-01-12 19:58:24 +0900

    add broadcast

--- src/screens/TradeScreen/useTrade.ts
+++ src/screens/TradeScreen/useTrade.ts
@@ -1,14 +1,13 @@
-import { useEffect, useMemo, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react';
 
 import BigNumber from 'bignumber.js';
 import { useTranslation } from 'react-i18next';
 
 import { TGasConfirmButtonFunctionParam } from '@@components/BasicComponents/GasFeeBoard/GasFeeBoard.type';
 import { MODAL_TYPES } from '@@components/BasicComponents/Modals/GlobalModal';
-import { getNetworkByBase, getNetworkConfig } from '@@constants/network.constant';
-import { IGasFeeInfo } from '@@domain/gas/GasService.type';
+import { getNetworkByBase } from '@@constants/network.constant';
 import { ISwapDto } from '@@domain/trade/repositories/tradeRepository.type';
-import { FetchPriceResponseDto } from '@@generated/generated-scheme-clutch';
+import { BroadcastTransactionDto, CreateNativeSwapTransactionResponseDto, FetchPriceResponseDto } from '@@generated/generated-scheme-clutch';
 import useSwapDataQuery from '@@hooks/queries/useSwapDataQuery';
 import { useDi } from '@@hooks/useDi';
 import gasStore from '@@store/gas/gasStore';
@@ -22,12 +21,13 @@ const useTrade = (fromToken: TokenDto | undefined, quoteData: FetchPriceResponse
   const { openModal, closeModal } = globalModalStore();
   const TransactionService = useDi('TransactionService');
   const WalletService = useDi('WalletService');
+  const TradeRepository = useDi('TradeRepository');
 
   const { t } = useTranslation();
-  const { to: spender, value, data: swapData, setState } = transactionRequestStore();
-  const { setState: setGasStore } = gasStore();
+  const { to: spender, value, setState } = transactionRequestStore();
+  const { setState: setGasStore, resetState: resetGasStore } = gasStore();
   const [swapDto, setSwapDto] = useState<ISwapDto | null>(null);
-  const [serverSentSwapData, setServerSentSwapData] = useState<string | null>(null);
+  const [serverSentSwapData, setServerSentSwapData] = useState<CreateNativeSwapTransactionResponseDto['tx'] | null>(null);
 
   useEffect(() => {
     getSwapDto();
@@ -36,6 +36,7 @@ const useTrade = (fromToken: TokenDto | undefined, quoteData: FetchPriceResponse
   const getSwapDto = async () => {
     if (!quoteData || !quoteData.fromToken?.address || !quoteData.toToken?.address) return;
     const wallet = await WalletService.getWalletInfo({ index: selectedWalletIndex[selectedNetwork], network: selectedNetwork });
+
     setSwapDto({
       fromTokenAddress: quoteData.fromToken.address,
       toTokenAddress: quoteData.toToken?.address,
@@ -48,30 +49,38 @@ const useTrade = (fromToken: TokenDto | undefined, quoteData: FetchPriceResponse
   useSwapDataQuery(selectedNetwork, swapDto, {
     onSuccess: (res) => {
       if (!res) return;
-      const { from, to, value, data } = res.tx;
-
+      const { from, to, value } = res.tx;
       const bnValue = new BigNumber(value);
-
       setGasStore({ isDataRequired: true });
-      setServerSentSwapData(data); //useTradeApprove에서 approveData를 사용하고나서야 data를 할당해야함
-      setState({ from, to, value: bnValue, toValid: true, valueValid: true }); //내가 보낸 value가 오는것 같은데 바꿔서 할당하는게 맞나? 비교 컨펌필요
+      setServerSentSwapData(res.tx);
+      setState({ from, to, value: bnValue, toValid: true, valueValid: true });
     },
   });
 
   useEffect(() => {
     if (isEnoughAllowance && serverSentSwapData) {
-      setState({ data: serverSentSwapData });
+      setState({ data: serverSentSwapData.data });
+      //useTradeApprove에서 approveData를 사용하고나서야 data를 할당해야함
     }
   }, [isEnoughAllowance, serverSentSwapData]);
 
+  const broadCastToServer = (param: BroadcastTransactionDto) => {
+    TradeRepository.broadcast(getNetworkByBase(selectedNetwork), param);
+  };
+
   const sendTradeTransaction = async (param: TGasConfirmButtonFunctionParam) => {
-    if (!spender || !param) return;
-    if (!value) return;
-    await TransactionService.sendTransaction({
+    if (!spender || !param || !serverSentSwapData || !value) return;
+    const hash = await TransactionService.sendTransaction({
       ...param,
       selectedNetwork: getNetworkByBase(selectedNetwork),
       selectedWalletIndex: selectedWalletIndex[getNetworkByBase(selectedNetwork)],
     });
+    if (!hash) {
+      console.log('fail send trade transaction');
+      return;
+    }
+    const nonce = (await TransactionService.getNonce(getNetworkByBase(selectedNetwork), hash)) ?? 0;
+    broadCastToServer({ ...serverSentSwapData, hash, nonce });
     closeModal();
     setGasStore({ isDataRequired: false });
   };
