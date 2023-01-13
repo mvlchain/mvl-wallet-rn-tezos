756eecbcffada766856a3196eaa96e2e845027a3
cutehackers <cutehackers@gmail.com>
2023-01-13 16:41:22 +0900

    fix: QR code in Dark mode

--- src/components/BasicComponents/QR/QRcode/QRcode.style.ts
+++ src/components/BasicComponents/QR/QRcode/QRcode.style.ts
@@ -1,7 +1,18 @@
 import styled from 'styled-components/native';
 
+import { getWidth, width } from '@@utils/ui';
+
 export const QRcodeWrapper = styled.View`
   justify-content: center;
   align-items: center;
   background-color: white;
+`;
+
+export const QRcodeDarkThemeWrapper = styled.View`
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-width: ${getWidth(3)}px;
+  border-color: ${({ theme }) => theme.color.white};
+  border-style: solid;
 `;
