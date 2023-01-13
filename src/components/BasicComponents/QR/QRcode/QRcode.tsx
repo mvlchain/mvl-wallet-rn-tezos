a633e4cb023ced7c750c231aca37836a2f7b147e
cutehackers <cutehackers@gmail.com>
2023-01-13 16:41:22 +0900

    fix: QR code in Dark mode

--- src/components/BasicComponents/QR/QRcode/QRcode.tsx
+++ src/components/BasicComponents/QR/QRcode/QRcode.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 
 import QRCode from 'react-native-qrcode-svg';
 
+import settingPersistStore from '@@store/setting/settingPersistStore';
 import { SCREEN_WIDTH } from '@@utils/ui';
 
 import * as S from './QRcode.style';
 import { IQRcode } from './QRcode.type';
 
 function QRcode({ qr }: IQRcode) {
-  return (
+  const { appTheme } = settingPersistStore();
+  return appTheme.value === 'light' ? (
     <S.QRcodeWrapper>
       <QRCode size={SCREEN_WIDTH * 0.6} value={qr} />
     </S.QRcodeWrapper>
+  ) : (
+    <S.QRcodeDarkThemeWrapper>
+      <QRCode size={SCREEN_WIDTH * 0.6} value={qr} />
+    </S.QRcodeDarkThemeWrapper>
   );
 }
 
