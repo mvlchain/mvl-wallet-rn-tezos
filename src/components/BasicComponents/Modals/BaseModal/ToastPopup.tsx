bf39556b38d5e314e2faaf652282715b083145c3
choichoikule <67899064+choichoikule@users.noreply.github.com>
2023-01-17 15:04:37 +0900

    useToast add

--- src/components/BasicComponents/Modals/BaseModal/ToastPopup.tsx
+++ src/components/BasicComponents/Modals/BaseModal/ToastPopup.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 
 import Toast from 'react-native-toast-message';
 
+import { TOAST_TYPE } from '@@constants/toastConfig.constant';
+
 import * as S from './Modal.style';
 
 export function ToastPopup() {
   const toastConfig = {
-    basic: (props: any) => (
+    [TOAST_TYPE.BASIC]: (props: any) => (
       <S.ToastPopupContainer>
         <S.ToastTextWrapper>
           <S.ToastText>{props.text1}</S.ToastText>
+        </S.ToastTextWrapper>
+      </S.ToastPopupContainer>
+    ),
+    [TOAST_TYPE.ERROR]: (props: any) => (
+      <S.ToastPopupContainer>
+        <S.ToastTextWrapper>
+          <S.ToastErrorText>{props.text1}</S.ToastErrorText>
         </S.ToastTextWrapper>
       </S.ToastPopupContainer>
     ),
