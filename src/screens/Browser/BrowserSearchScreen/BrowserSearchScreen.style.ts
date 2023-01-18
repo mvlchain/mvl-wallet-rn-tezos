f7939e4bd5f3660a8811e8e97c21a7f34b5de765
cudymvl <cudy.kim@mvlchain.io>
2023-01-18 16:11:57 +0900

    feat: delete search 버튼

--- src/screens/Browser/BrowserSearchScreen/BrowserSearchScreen.style.ts
+++ src/screens/Browser/BrowserSearchScreen/BrowserSearchScreen.style.ts
@@ -1,4 +1,4 @@
-import { TextInput } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
 import styled from 'styled-components/native';
 
 import { width } from '@@utils/ui';
@@ -9,19 +9,27 @@ export const Container = styled.View`
 `;
 
 export const SearchContainer = styled.View`
-  padding: ${width * 24}px;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  padding: ${width * 24}px;
+`;
+
+export const SearchWrapper = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  margin-right: ${width * 12}px;
+  border-radius: ${width * 8}px;
+  border-width: ${width * 1}px;
+  border-color: ${({ theme }) => theme.color.highlight};
 `;
 
 export const SearchInput = styled.TextInput`
   flex: 1;
   padding: ${width * 14}px ${width * 16}px;
-  margin-right: ${width * 24}px;
-  border-radius: ${width * 8}px;
-  border-width: ${width * 1}px;
-  border-color: ${({ theme }) => theme.color.highlight};
+  ${({ theme }) => theme.font.Label.md};
+  color: ${({ theme }) => theme.color.blackWhite};
 ` as unknown as typeof TextInput;
 
 export const ContentContainer = styled.View`
@@ -37,3 +45,7 @@ export const EmptyHistoryText = styled.Text`
   ${({ theme }) => theme.font.Label.md};
   color: ${({ theme }) => theme.color.grey500};
 `;
+
+export const inlineStyles = StyleSheet.create({
+  marginProvider: { marginRight: 8 },
+});
