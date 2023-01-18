6018cc6b884c4a6ba2de79aa5abbaf38bf4ad4cf
cudymvl <cudy.kim@mvlchain.io>
2023-01-18 15:45:27 +0900

    feat: style add

--- src/screens/Browser/BrowserDappScreen/BrowserDappScreen.style.ts
+++ src/screens/Browser/BrowserDappScreen/BrowserDappScreen.style.ts
@@ -1,1 +1,16 @@
 import styled from 'styled-components/native';
+
+import { width } from '@@utils/ui';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: ${width * 24}px;
+`;
+
+export const Pressable = styled.Pressable`
+  flex: 1;
+  justify-content: center;
+`;
