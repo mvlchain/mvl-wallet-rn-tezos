d87e754bb051c899dd1a264c22b3cfbfe6404761
cudymvl <cudy.kim@mvlchain.io>
2023-01-19 18:08:28 +0900

    fix: add memo in hook

--- src/screens/Browser/BrowserSearchScreen/BrowserSearchHistoryItem/useBrowserSearchHistoryItem.tsx
+++ src/screens/Browser/BrowserSearchScreen/BrowserSearchHistoryItem/useBrowserSearchHistoryItem.tsx
@@ -1,3 +1,5 @@
+import React, { useMemo } from 'react';
+
 import useBrowserHistoryStore from '@@store/dapp/useBrowserHistoryStore';
 
 import * as S from './BrowserSearchHistoryItem.style';
@@ -6,24 +8,27 @@ import { IUseBrowserSearchHistoryItemParam } from './BrowserSearchHistoryItem.ty
 const useBrowserSearchHistoryItem = ({ title }: IUseBrowserSearchHistoryItemParam) => {
   const { searchValue } = useBrowserHistoryStore();
 
-  const renderTitle = () => {
-    if (searchValue.length > 0) {
-      const index = title.toLowerCase().indexOf(searchValue.toLowerCase());
-      if (index > -1) {
-        const before = title.substring(0, index);
-        const search = title.substring(index, index + searchValue.length);
-        const after = title.substring(index + searchValue.length);
-        return (
-          <S.Title>
-            {before}
-            <S.Title isHighlight={true}>{search}</S.Title>
-            {after}
-          </S.Title>
-        );
+  const renderTitle = useMemo(
+    () => () => {
+      if (searchValue.length > 0) {
+        const index = title.toLowerCase().indexOf(searchValue.toLowerCase());
+        if (index > -1) {
+          const before = title.substring(0, index);
+          const search = title.substring(index, index + searchValue.length);
+          const after = title.substring(index + searchValue.length);
+          return (
+            <S.Title>
+              {before}
+              <S.Title isHighlight={true}>{search}</S.Title>
+              {after}
+            </S.Title>
+          );
+        }
       }
-    }
-    return <S.Title>{title}</S.Title>;
-  };
+      return <S.Title>{title}</S.Title>;
+    },
+    [title, searchValue]
+  );
 
   return { renderTitle };
 };
