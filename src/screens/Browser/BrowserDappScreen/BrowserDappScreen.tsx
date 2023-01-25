d11a659833bb80c70496e6309de29d7944229517
cudymvl <cudy.kim@mvlchain.io>
2023-01-25 11:45:52 +0900

    feat: dapp refresh기능 - 아이콘 수정하기

--- src/screens/Browser/BrowserDappScreen/BrowserDappScreen.tsx
+++ src/screens/Browser/BrowserDappScreen/BrowserDappScreen.tsx
@@ -12,15 +12,17 @@ import useBrowserDappScreen from './useBrowserDappScreen';
 // TODO: Main(dapp)마무리 시 BrowserDappScreen로 코드 이사시키기(진과 작업이 꼬일까봐 일단 wrapper로 만들어뒀습니다)
 function BrowserDappScreen(props: IBrowserDappScreenProps) {
   const CloseIcon = useAssetFromTheme(CloseBlackIconLight, CloseBlackIconDark);
-  const { onPressClose } = useBrowserDappScreen();
+  const { webviewRef, onPressRefresh, onPressClose } = useBrowserDappScreen();
   return (
     <S.Container>
       <S.Header>
         <S.Pressable onPress={onPressClose}>
           <CloseIcon />
+          {/* TODO: refresh 아이콘으로 수정 */}
+          <CloseIcon onPress={onPressRefresh} />
         </S.Pressable>
       </S.Header>
-      <Main />
+      <Main webviewRef={webviewRef} />
     </S.Container>
   );
 }
