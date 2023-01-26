7b475f86f04e98250a58d38e12c6da1c8baf6c4a
Ye Won Choi <67899064+choichoikule@users.noreply.github.com>
2023-01-27 00:10:59 +0900

    gasfee prop onConfirm type fix

--- src/components/BasicComponents/GasFeeBoard/GasFeeBoard.type.ts
+++ src/components/BasicComponents/GasFeeBoard/GasFeeBoard.type.ts
@@ -3,12 +3,14 @@ import { TransferParams } from '@taquito/taquito';
 import BigNumber from 'bignumber.js';
 import { BytesLike } from 'ethers';
 
+import { TGasLevel } from '@@constants/gas.constant';
+
 export interface IUseGasProps {
   to: string | null;
   value?: BigNumber | null;
   data?: BytesLike | null;
   isValidInput: boolean;
-  onConfirm: (params: TransactionRequest | TransferParams) => void;
+  onConfirm?: (params: TransactionRequest | TransferParams, gasSettingInfo?: IGasSettingInfo) => void;
 }
 export interface IEVMGasComponentProps extends IUseGasProps {
   isRevision: boolean;
@@ -27,4 +29,9 @@ export interface IGasInjectParamToOnConfirm {
   to: string;
   value?: BigNumber | null;
   sendParam: TransactionRequest | TransferParams;
+}
+
+export interface IGasSettingInfo {
+  advanced: boolean;
+  level: TGasLevel;
 }
