5ed3a1ccb438a4e4e186517cc97f2d10542b5dde
cutehackers <cutehackers@gmail.com>
2023-01-26 13:50:01 +0900

    fix: EventDetails screen update issue when re-visited

---
+++ src/hooks/useAppStateChange.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from 'react';
+
+import { AppState, AppStateStatus } from 'react-native';
+
+/**
+ * App visibility observer hook.
+ * This will not work in the very first time when the component renders
+ *
+ * @example
+ * useAppStateChange(isAppStateVisible => {
+ *   isAppStateVisible == true if app is in foreground, false if app is in background
+ *   console.log(isAppStateVisible);
+ * });
+ *
+ * @param onAppStateChanged
+ */
+export const useAppStateChange = (onAppStateChanged: (isAppStateVisible: boolean) => void) => {
+  const currentAppState = useRef(AppState.currentState);
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onAppStateChanging);
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const onAppStateChanging = (nextAppState: AppStateStatus) => {
+    if (currentAppState.current.match(/inactive|background/) && nextAppState === 'active') {
+      // foreground
+      onAppStateChanged(true);
+    } else if (appState === 'active' && nextAppState.match(/inactive|background/)) {
+      // background
+      onAppStateChanged(false);
+    }
+    currentAppState.current = nextAppState;
+    setAppState(nextAppState);
+  };
+};
