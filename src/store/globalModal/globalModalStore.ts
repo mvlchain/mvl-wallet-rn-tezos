3305ee4be8272958defb4e209f515a7592dc9af2
cutehackers <cutehackers@gmail.com>
2023-01-27 15:19:02 +0900

    feat: toast a error message when failed to run sendTranasction

--- src/store/globalModal/globalModalStore.ts
+++ src/store/globalModal/globalModalStore.ts
@@ -2,6 +2,11 @@ import zustandFlipper from 'react-native-flipper-zustand';
 import create from 'zustand';
 
 import { MODAL_TYPE, IGlobalModalStore, MODAL_PROPS } from './globalModalStore.type';
+
+/**
+ * TODO:
+ *  - closeModal을 실행할 경우 파라미터로 모달 아이디를 주고, 결과를 받을 수 있도록 해보자
+ */
 const globalModalStore = create<IGlobalModalStore>(
   zustandFlipper(
     (set) => ({
